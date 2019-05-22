3af39f013e590b501727ebe921b7641b34dc7800
nkpremices <premices.tuvere@gmail.com>
2019-05-22 14:15:50 +0200

    [Chore #166175223] Give the environnement file to the project

---
+++ server/configs/environments.js
@@ -0,0 +1,92 @@
+/* eslint-disable import/no-mutable-exports */
+/**
+ * Create and export environment variables
+ * @name environments
+ */
+
+import dotenv from 'dotenv';
+
+let environment;
+
+dotenv.config();
+
+// Initializing env variables
+const appPort = process.env.PORT;
+const saltingRounds = process.env.SALTING_ROUNDS;
+const jwtKey = process.env.JWT_KEY;
+const dbUser = process.env.DB_USER;
+const dbHost = process.env.DB_HOST;
+const devDbName = process.env.DEV_DB_NAME;
+const testDbName = process.env.TEST_DB_NAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT;
+const envName = process.env.NODE_ENV;
+
+/**
+ * An object to contain all the needed environements
+ * @property {object} test - The test env object
+ * @property {object} development - The development env object
+ * @property {object} staging - The staging env object
+ */
+const environments = {
+    test: {
+        name: envName,
+        app: {
+            port: appPort,
+        },
+        database: {
+            user: dbUser,
+            host: dbHost,
+            name: testDbName,
+            password: dbPassword,
+            port: dbPort,
+        },
+        saltingRounds,
+        jwtKey,
+    },
+
+    development: {
+        name: envName,
+        app: {
+            port: appPort,
+        },
+        database: {
+            user: dbUser,
+            host: dbHost,
+            name: devDbName,
+            password: dbPassword,
+            port: dbPort,
+        },
+        saltingRounds,
+        jwtKey,
+    },
+
+    staging: {
+        name: envName,
+        app: {
+            port: appPort,
+        },
+        database: {
+            user: dbUser,
+            host: dbHost,
+            name: testDbName,
+            password: dbPassword,
+            port: dbPort,
+        },
+        saltingRounds,
+        jwtKey,
+    },
+};
+
+// Determine which environment we are in
+const currentEnvironment = process.env.NODE_ENV.toLowerCase() || '';
+
+/* Check that the current environment is one the envs
+   defined above, if not default to dev
+*/
+if (environments[currentEnvironment]) {
+    environment = environments[currentEnvironment];
+} else environment = environments.development;
+
+// Export the environnement
+export default environment;
