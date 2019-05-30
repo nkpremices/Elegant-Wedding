907ecb9f91f374366ba87bbafa6bc9fca41e0b65
nkpremices <premices.tuvere@gmail.com>
2019-05-30 23:27:47 +0200

    [Feature #166135737] Add a controller to create a package

--- server/api/controllers/admin.js
+++ server/api/controllers/admin.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 import models, { sequelize } from '../../db/models';
-
+import sendError from '../../helpers/send.error';
 
 const services = {
-    async createService(req, res) {
+    createService: async (req, res) => {
         const { name } = req.body;
         const { type } = req.user;
         if (type === 'admin') {
@@ -19,6 +20,25 @@ const services = {
         return res.status(403).json({
             message: 'You do not have the right to create a service!',
         });
+    },
+    createPackage: async (req, res) => {
+        // Initialising variables
+        const result = {};
+        const status = 201;
+        let tempPackage;
+
+        try {
+            tempPackage = await models.Packages.create(req.body);
+            // Sending the result
+            result.status = status;
+            result.message = 'Package registered successfully';
+
+            result.data = tempPackage.dataValues;
+
+            res.status(status).json(result);
+        } catch (error) {
+            sendError(205, result, res, error);
+        }
     },
 
 };
