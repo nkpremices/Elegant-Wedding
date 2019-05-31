a4a7752b7096649c21fb959c8abf2a99becf83b4
nkpremices <premices.tuvere@gmail.com>
2019-05-31 12:54:59 +0200

    [Feature #166135734] Give the payment info endpoint to the service provider router

--- server/api/routes/serviceProvider.js
+++ server/api/routes/serviceProvider.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import bookings from '../controllers/bookings';
+import paymentController from '../controllers/payments';
+import validateBody from '../../middlewares/validate.body';
+import authorization from '../../middlewares/authorization';
+import authenticateUser from '../../middlewares/validate.auth.routes';
 
 const serviceProviderRouter = Router();
 
 serviceProviderRouter.get('/bookings/:postId', bookings.bookingsPerPost);
 serviceProviderRouter.get('/bookings/posts/:header',
     bookings.bookingsPerHeader);
+
+serviceProviderRouter.post('/payment',
+    authorization,
+    authenticateUser.authoriseUser('provider'),
+    validateBody(true, 'payment'),
+    paymentController.imputPaymentInfo);
 
 export default serviceProviderRouter;
