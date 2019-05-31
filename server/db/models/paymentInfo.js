8ccfef3342ff47570c73e1d3d9f2b872d2badda6
nkpremices <premices.tuvere@gmail.com>
2019-05-31 12:54:04 +0200

    [Feature #166135734] Update the payment info model

--- server/db/models/paymentInfo.js
+++ server/db/models/paymentInfo.js
@@ -3,7 +3,7 @@
 const paymentInfos = (sequelize, DataTypes) => {
     const PaymentInfos = sequelize.define('paymentInfos', {
         fullName: DataTypes.STRING,
-        expirationDate: DataTypes.STRING,
+        expirationDate: DataTypes.DATE,
         cvc: DataTypes.INTEGER,
         accountNumber: DataTypes.STRING,
     });
